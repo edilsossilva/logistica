c9b1131b4dc21cef75d5556d9819f243c73b7a0d
Edilsonssilva <edilsossilva@gmail.com>
2021-08-22 22:46:49 -0300

    Dashboard
    Criado arquivo dashboard e botões que linkam home e dashboard.

--- slogweb/core/views.py
+++ slogweb/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request, template_name = 'core/home.html'):
     return render(request, template_name)
+
+
+def dashboard(request, template_name = 'core/dashboard.html'):
+    return render(request, template_name)
